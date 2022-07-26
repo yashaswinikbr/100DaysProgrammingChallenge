ed958c29a8051891276a08ca9dd4d29228adf3e9
yashaswinikbr <109444074+yashaswinikbr@users.noreply.github.com>
2022-07-26 18:12:36 -0400

    Time: 6 ms (63.45%), Space: 45.3 MB (30.70%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] str1=s.toCharArray();
+        char[] str2=t.toCharArray();
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+        
+        return Arrays.equals(str1,str2);
+        
+        
+    }
+}
+
+// M 1- figure out all the permutations of a string , take O(n!) time
+// M 2 - sort the 2 strings, acb, bac= abc,abc , true.
+//       to convert to string and sort the strings= O(nlogn), space=O(N)
+//       to compare the 2 strings= O(N), space=O(1)
+
+
+
+
+
+// HashMap <String, Integer> h=new HashMap<>();
+//         for(int i=0;i<s.length();i++)
+//         {
+//             h.put(s.charAt(i),0);
+//         }
+//         for(int i=0;i<t.length();i++)
+//         {
+//             if(h.containsKey(t.charAt(i)))
+//                 return true;
+            
+//         }
+//         return false;
