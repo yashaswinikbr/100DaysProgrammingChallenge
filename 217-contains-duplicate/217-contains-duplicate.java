0bbb59cde400c1d585d035efd8c0275452d4cc57
yashaswinikbr <109444074+yashaswinikbr@users.noreply.github.com>
2022-07-26 08:55:54 -0400

    Time: 14 ms (52.64%), Space: 68.2 MB (74.09%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -1,15 +1,16 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length-1;i++)
+        HashMap<Integer,Integer > h=new HashMap<>();
+        for(int i=0;i<nums.length;i++)
         {
-            if(nums[i]==nums[i+1])
+            if(h.containsKey(nums[i]))
                 return true;
+            h.put(nums[i],0);
         }return false;
     }
 }
 
-//M 1- take 1 element traverse it whole and check if exists. O(n^2)
+// M 1- take 1 element traverse it whole and check if exists. O(n^2)
 // for(int i=0;i<nums.length;i++)
 //         {  for(int j=i+1;j<nums.length;j++)
 //             { if(nums[i]==nums[j])
@@ -19,4 +20,12 @@ class Solution {
 // return false;
 
 
-// M2- sort and check if adjacent elements are repeating 
+// M2- sort and check if adjacent elements are repeating O(n log n)
+// Arrays.sort(nums);
+//         for(int i=0;i<nums.length-1;i++)
+//         {
+//             if(nums[i]==nums[i+1])
+//                 return true;
+//         }return false;
+
+//M3 - use hashmap and check for containsKey. O(n)
