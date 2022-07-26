63369996e6db4e13a2c2c881922b426e4845b3ff
yashaswinikbr <109444074+yashaswinikbr@users.noreply.github.com>
2022-07-26 13:59:15 -0400

    Time: 2 ms (87.89%), Space: 58.3 MB (17.13%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -1,22 +1,20 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
+        //FOR SPACE + O(1)
+        
           int answer[]=new int[nums.length];
-          int L[]=new int[nums.length];
-          int R[]=new int[nums.length];
-        L[0]=1;
+       answer[0]=1;
         for(int i=1;i<nums.length;i++)
         {
-            L[i]=L[i-1]*nums[i-1];
+            answer[i]=answer[i-1]*nums[i-1];
         }
-        R[nums.length-1]=1;
-         for(int i=nums.length-2;i>=0;i--)
+        int R=1;
+         for(int i=nums.length-1;i>=0;i--)
         {
-            R[i]=R[i+1]*nums[i+1];
+            answer[i]=answer[i]*R;
+             R=nums[i]*R;
         }
-         for(int i=0;i<nums.length;i++)
-         {
-             answer[i]=L[i]*R[i];
-         }
+       
     return answer;
     }
 }
@@ -39,4 +37,23 @@ class Solution {
 
 // M2 use L[] , R[] to store the PrefixProduct and suffixprod of the nums[i]
 //O(n),space-O(n)
+
+// int answer[]=new int[nums.length];
+//           int L[]=new int[nums.length];
+//           int R[]=new int[nums.length];
+//         L[0]=1;
+//         for(int i=1;i<nums.length;i++)
+//         {
+//             L[i]=L[i-1]*nums[i-1];
+//         }
+//         R[nums.length-1]=1;
+//          for(int i=nums.length-2;i>=0;i--)
+//         {
+//             R[i]=R[i+1]*nums[i+1];
+//         }
+//          for(int i=0;i<nums.length;i++)
+//          {
+//              answer[i]=L[i]*R[i];
+//          }
+//     return answer;
    
