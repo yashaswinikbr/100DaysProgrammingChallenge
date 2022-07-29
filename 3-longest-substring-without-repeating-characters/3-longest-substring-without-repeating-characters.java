7955425f4896b75b63167c877c52bc73cb17f314
yashaswinikbr <109444074+yashaswinikbr@users.noreply.github.com>
2022-07-28 20:20:06 -0400

    Time: 5 ms (92.27%), Space: 42.2 MB (95.04%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -1,25 +1,19 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) { 
-       int[] chars= new int[128]; 
-       int left=0;
-        int right=0;
-        int res=0;
-        while(right<s.length())
-        {
-            char r=s.charAt(right);
-            chars[r]++;
-            
-            while(chars[r]>1)
-            {
-                char l=s.charAt(left);
-                chars[l]--;
-                left++;
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>(); // current index of character
+        // try to extend the range [i, j]
+        for (int j = 0, i = 0; j < n; j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)), i);
             }
-            res=Math.max(res,right-left+1);
-            right++;
+            ans = Math.max(ans, j - i + 1);
+            map.put(s.charAt(j), j + 1);
         }
-       return res; 
+        return ans;
+      
 }
+    
 }
 
 // M 1- enumerate/traverse all the elements and check for repetition, and increment at ascii val of char[], use 2 pointer technique. O(n^3)
@@ -50,6 +44,25 @@ class Solution {
     
     
 //     M 2- sliding window- traverse and store freq with right pointer, if freq>1 decrement left pointer an delete the left char--- O(2n) 
+//  int[] chars= new int[128]; 
+//        int left=0;
+//         int right=0;
+//         int res=0;
+//         while(right<s.length())
+//         {
+//             char r=s.charAt(right);
+//             chars[r]++;
+            
+//             while(chars[r]>1)
+//             {
+//                 char l=s.charAt(left);
+//                 chars[l]--;
+//                 left++;
+//             }
+//             res=Math.max(res,right-left+1);
+//             right++;
+//         }
+//        return res; 
        
         
 //     M 3- We use HashSet to store the characters in current window [i, j)[i,j) (j = ij=i initially). Then we slide the index jj to the right. If it is not in the HashSet, we slide jj further. Doing so until s[j] is already in the HashSet. At this point, we found the maximum size of substrings without duplicate characters start with index ii. If we do this for all ii, we get our answer.
