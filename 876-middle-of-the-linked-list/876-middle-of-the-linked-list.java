5166585c5530976b3276e6b73d187cded2f4b01e
yashaswinikbr <109444074+yashaswinikbr@users.noreply.github.com>
2022-08-14 16:20:28 -0400

    Time: 0 ms (100.00%), Space: 41.7 MB (47.21%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.java
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.java
@@ -10,25 +10,37 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        int count=0, midpos=0;
-       ListNode temp=head;
-        while(temp.next != null)
+       ListNode slow=head;
+        ListNode fast=head;
+        while(fast !=null && fast.next !=null )
         {
-            temp=temp.next;
-            count++;
+            slow= slow.next;
+            fast=fast.next.next;
         }
-        if(count%2==0)
-            midpos=count/2;
-        else
-            midpos=count/2 + 1;
-        int c=0;
-        while(c<midpos)
-        {
-          head=head.next; 
-            c++;
-        }
-        return head;
+        
+
+      return slow;  
     }
 }
 
 // M 1- iterate over the list and find count, if even- mid=count/2, else count/2+1
+    // int count=0, midpos=0;
+    //    ListNode temp=head;
+    //     while(temp.next != null)
+    //     {
+    //         temp=temp.next;
+    //         count++;
+    //     }
+    //     if(count%2==0)
+    //         midpos=count/2;
+    //     else
+    //         midpos=count/2 + 1;
+    //     int c=0;
+    //     while(c<midpos)
+    //     {
+    //       head=head.next; 
+    //         c++;
+    //     }
+    //     return head;
+
+//M 2- 2 pointer approach- slow and fast pointer
