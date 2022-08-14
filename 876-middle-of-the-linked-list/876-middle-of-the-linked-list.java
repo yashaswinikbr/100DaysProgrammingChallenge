d972212da578c58c5d2ea73ef7ff69772da5e278
yashaswinikbr <109444074+yashaswinikbr@users.noreply.github.com>
2022-08-14 15:57:26 -0400

    Time: 0 ms (100.00%), Space: 41.5 MB (55.81%) - LeetHub

---
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        int count=0, midpos=0;
+       ListNode temp=head;
+        while(temp.next != null)
+        {
+            temp=temp.next;
+            count++;
+        }
+        if(count%2==0)
+            midpos=count/2;
+        else
+            midpos=count/2 + 1;
+        int c=0;
+        while(c<midpos)
+        {
+          head=head.next; 
+            c++;
+        }
+        return head;
+    }
+}
+
+// M 1- iterate over the list and find count, if even- mid=count/2, else count/2+1
