b5f4ee47e1b863cae8eeffb7efbf7d2ec123735d
yashaswinikbr <109444074+yashaswinikbr@users.noreply.github.com>
2022-09-04 13:27:40 -0400

    Time: 1 ms (63.87%), Space: 42.6 MB (31.66%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -29,3 +29,7 @@ class Solution {
         return areMirrorImage(root.left,root.right);
     }
 }
+
+
+
+// we can also use iteration with the aid of a queue. Each two consecutive nodes in the queue should be equal, and their subtrees a mirror of each other. Initially, the queue contains root and root. Then the algorithm works similarly to BFS, with some key differences. Each time, two nodes are extracted and their values compared. Then, the right and left children of the two nodes are inserted in the queue in opposite order. The algorithm is done when either the queue is empty, or we detect that the tree is not symmetric (i.e. we pull out two consecutive nodes from the queue that are unequal).
