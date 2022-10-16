a99b8be35ddadba3ed6bd7f838444f32c21e08e7
yashaswinikbr <109444074+yashaswinikbr@users.noreply.github.com>
2022-10-16 11:17:42 -0400

    Added solution - LeetHub

--- Key Pair - GFG/key-pair.java
+++ Key Pair - GFG/key-pair.java
@@ -39,35 +39,42 @@ class Solution {
     boolean hasArrayTwoCandidates(int arr[], int n, int x) {
         
         Arrays.sort(arr);
-        for(int i=0;i<n;i++)
-        {
-            int y=x-arr[i];
-            int low=i+1;
-            int high=n-1;
-            while(low<=high)
-            {
+        // for(int i=0;i<n;i++)
+        // {
+        //     int y=x-arr[i];
+        //     int low=i+1;
+        //     int high=n-1;
+        //     while(low<=high)
+        //     {
                 
-                int mid=(low+high)/2;
-                if(arr[mid]==y) return true;
-                else if(arr[mid]<y) low=mid+1;
-                else high=mid-1;
-            }
-            
-        }
-        return false;
-    }
-        //  int i,findElement;
-        // HashMap<Integer,Integer> map = new HashMap<>();
-       
-        // for( i=0;i<n;i++){
-        //     findElement = x-arr[i];
-        //     if(map.containsKey(findElement)){
-        //         return true;
-        //     }else{
-        //         map.put(arr[i],i);
+        //         int mid=(low+high)/2;
+        //         if(arr[mid]==y) return true;
+        //         else if(arr[mid]<y) low=mid+1;
+        //         else high=mid-1;
         //     }
+            
         // }
-        // return false;
+    //     for( int i=0;i<n;i++)
+    //     {
+    //         for(int j=n-1;j>1;j--)
+    //         {
+    //             if(arr[i]+arr[j]==x) return true;
+    //         }
+    //     }
+    //     return false;
+    // }
+         int i,findElement;
+        HashMap<Integer,Integer> map = new HashMap<>();
+       
+        for( i=0;i<n;i++){
+            findElement = x-arr[i];
+            if(map.containsKey(findElement)){
+                return true;
+            }else{
+                map.put(arr[i],i);
+            }
+        }
+         return false;
         //  for(int i=0;i<n;i++)  //M1
         //  {
         //      for(int j=0;j<n;j++)
@@ -77,5 +84,5 @@ class Solution {
         //      }
         //  }
         
-    
+    } 
 }
